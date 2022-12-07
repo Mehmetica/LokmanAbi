f32dec77804ace521db2de33a37d83c15ee3f456
Mehmet Gezer <mehmetica74@gmail.com>
2022-12-07 15:13:11 +0300

    Proje basladı

---
+++ src/AnaClass/TC1.java
@@ -0,0 +1,11 @@
+package AnaClass;
+
+public class TC1 {
+    public static void main(String[] args) {
+        System.out.println("Lokamn abi");
+        System.out.println("Gezer");
+
+
+
+    }
+}
